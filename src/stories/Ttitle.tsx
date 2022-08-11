153d545f9ee466b43757231aeda1207d136d2778
lnara404 <naas0974@gmail.com>
2022-08-11 10:41:21 +0900

    :lipstick: Test stylelint

---
+++ src/stories/Ttitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "@emotion/styled";
+
+const TitleStyle=styled.h2`
+  display: inline-block;
+  color: black;
+`;
+
+const Title = () => {
+  <TitleStyle>타이틀</TitleStyle>
+};
+
+export default Title;
